976165ddcdcc4f2e66fa996fe590376fd6605921
Naoki Ohta <naoki@HitoshiAraki-no-MacBook-Pro.local>
2016-09-30 14:38:48 +0900

    Position をアウトプットする、インポートする機能を実装

---
+++ Assets/Script/FileInScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class FileInScript : MonoBehaviour {
+	private StreamReader positionReader;
+
+	void OnEnable() {
+		positionReader = new StreamReader("PositionLog.txt");
+	}
+	void OnDisable() {
+		positionReader.Close();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!positionReader.EndOfStream) {
+			var _str = positionReader.ReadLine().Split(',');
+			var position = new Vector3 (float.Parse(_str[0]),float.Parse(_str[1]),float.Parse(_str[2]));
+			this.gameObject.transform.position = position;
+		}
+	
+	}
+}
---
+++ Assets/Script/FileOutScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class FileOutScript : MonoBehaviour {
+	private StreamWriter positionLog;
+
+	void OnEnable() {
+		positionLog = new StreamWriter("PositionLog.txt",false);
+	}
+
+	void OnDisable() {
+		positionLog.Close();
+	}
+
+	// Use this for initialization
+	void Start () {
+		WriteText("test_file.txt","hogehoge");
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 position = this.gameObject.transform.position;
+		WriteLine(positionLog,GetPositonString(position));
+	
+	}
+
+	// [_filePath]
+	// Running in Editor, root path is the project root path.
+	// Running in Application, root path is the Application existing folder path.
+	public void WriteText( string _filePath, string _contents ){
+		StreamWriter sw;
+		sw = new StreamWriter(_filePath, false);
+		sw.WriteLine(_contents);
+		sw.Close();
+	}
+
+	private void WriteLine(StreamWriter _sw, string _line ) {
+		_sw.WriteLine(_line);
+	}
+
+	private string GetPositonString(Vector3 _position) {
+		return _position.x + "," + _position.y + "," + _position.z;
+	}
+}
